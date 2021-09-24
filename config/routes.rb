81e266214d777aaae1a5f1cb1e3fe1708dccf225
gypsyrossel <gypsyrossel@gmail.com>
2021-09-23 21:21:29 -0300

    Agregando ruta, accion y vista show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'articles#index'
+  # get '/articles', to: 'articles#index'  ----> puedo volver a agregar esta ruta
   get '/articles/:id', to: 'articles#show'
 end 

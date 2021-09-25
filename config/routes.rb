df80baff44c7b0e0e9926a486cc1d624e738272f
gypsyrossel <gypsyrossel@gmail.com>
2021-09-25 19:49:37 -0300

    Usando metodo resources para generar rutas rest del recurso articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'articles#index'
   # get '/articles', to: 'articles#index'  ----> puedo volver a agregar esta ruta
-  get '/articles/:id', to: 'articles#show'
+  # get '/articles/:id', to: 'articles#show'
+  resources :articles
 end 

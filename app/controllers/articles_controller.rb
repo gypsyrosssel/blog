c797d5fa66642fdd44c67eb311f64f06a14614fa
gypsyrossel <gypsyrossel@gmail.com>
2021-09-26 21:07:06 -0300

    Creando vista y acciones para actualizar articulo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,13 +13,27 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-
     if @article.save
-      redirect_to @article
+      redirect_to article_path(@article)
     else
       render :new
     end
   end  
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end  
+
   private
    def article_params
     params.require(:article).permit(:title, :body)

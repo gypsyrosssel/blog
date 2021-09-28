15660b57a75a81bd6a0c9cb07bcd63e8ecc29894
gypsyrossel <gypsyrossel@gmail.com>
2021-09-28 16:33:43 -0300

    Agregando rutas para comentarios

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'articles#index'
   # get '/articles', to: 'articles#index'  ----> puedo volver a agregar esta ruta
   # get '/articles/:id', to: 'articles#show'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end 
